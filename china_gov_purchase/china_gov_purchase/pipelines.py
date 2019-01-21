0eac231560a58cde1a74fcba5f0e6b35078bdcaa
winkboy <ramsey_leung@163.com>
2019-01-21 14:28:12 +0800

    v2.3
    中国政府采购网中对pipeline的修改

--- china_gov_purchase/china_gov_purchase/pipelines.py
+++ china_gov_purchase/china_gov_purchase/pipelines.py
@@ -33,8 +33,8 @@ class ChinaMobile(object):
             if item['addr_id'] != '' and item['title'] != '' and item['url'] != '' and item['intro'] != '' and item['web_time'] != '' :
                 item['web_time'] = int(time.mktime(time.strptime(item['web_time'], "%Y-%m-%d")))
                 # 正式上传到服务器
-                sql = "INSERT INTO ztb_info_25 (itemid,catid,title,style,addtime,adddate,areaid,status,linkurl,content) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (
-                'NULL', item['type_id'], item['title'], item['source_name'], item['time'], item['web_time'],
+                sql = "INSERT INTO ztb_info_25 (catid,title,style,addtime,adddate,areaid,status,linkurl,content) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (
+                    item['type_id'], item['title'], item['source_name'], item['time'], item['web_time'],
                 item['addr_id'], 3, item['url'], pymysql.escape_string(item['intro']))
 
                 # 单机测试
