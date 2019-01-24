3b78aed6d8958663460d3fa80d238981d2ea2d57
winkboy <ramsey_leung@163.com>
2019-01-24 19:39:46 +0800

    update_new_spider
    更新了一个新爬虫

---
+++ china_canton_railway/china_canton_railway/spiders/yueyang_city_gov_spider.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from copy import deepcopy
+import time
+import re
+from utils.STMP import send_mail_when_error
+from utils.city_data import get_city_dict
+from utils.Regular_Expression import regularExpression, category
+
+# http://www.yueyang.gov.cn/cxjs/zbtb/default.htm @ 岳阳市人民政府
+class yueyangSpiderSpider(scrapy.Spider):
+    name = 'yueyang_city_gov_spider'
+    allowed_domains = ['yueyang.gov.cn']
+
+    def __init__(self):
+
+        self.city_dict = get_city_dict()
+        self.regularExpression = regularExpression
+        self.category = category
+
+        self.govPurchase_baseUrl = 'http://www.yueyang.gov.cn/cxjs/zbtb/'
+        self.construction_baseUrl = 'http://www.yueyang.gov.cn/cxjs/12112/24215/24216/'
+
+        self.error_count = 0
+
+        self.start_urls = [
+            # ('招标结果', 'http://www.yueyang.gov.cn/cxjs/zbtb/default.jsp?pager.offset={}&pager.desc=false', 3),
+            # ('招标结果', 'http://www.yueyang.gov.cn/cxjs/12112/24215/24216/default.jsp?pager.offset={}&pager.desc=false', 3),
+            ('招标结果', 'http://www.yueyang.gov.cn/cxjs/12112/24215/24217/default.jsp?pager.offset={}&pager.desc=false', 3),
+        ]
+
+        self.headers = {
+            'Connection': 'keep-alive',
+        }
+
+    def start_requests(self):
+        for url_info in self.start_urls:
+            # 构造分页列表 index_1为第二页 index_2为第三页
+            urls = [url_info[1].format(i * 15) for i in range(0, url_info[2])]
+            for url in urls:
+                items = {}
+                items['type_id'] = self.category[url_info[0]]
+                yield scrapy.Request(url, callback=self.parse, meta={"items": deepcopy(items)}, headers = self.headers)
+
+    def parse(self, response):
+
+        items = response.meta['items']
+        # 获取所有招标信息的li标签
+        all_lis = response.xpath('//div[@style="display: block;"]//li')
+
+        for each_li in all_lis:
+            items['title'] = ''
+            items['url'] = ''
+            items['web_time'] = ''
+            items['intro'] = ''
+            items['addr_id'] = ''
+
+            try:
+                items['title'] = each_li.xpath('./a/text()').extract_first()
+            except:
+                pass
+
+            try:
+                if 'zbtb' in response.url:
+                    items['url'] = each_li.xpath('./a/@href').extract_first()
+                elif '24216' in response.url:
+                    items['url'] = each_li.xpath('./a/@href').extract_first()
+                elif '24217' in response.url:
+                    items['url'] = each_li.xpath('./a/@href').extract_first()
+            except:
+                msg = self.name + ', 该爬虫详情页获取url失败'
+                send_mail_when_error(msg)
+                self.error_count += 1
+                if self.error_count > 3:
+                    quit()
+                    msg = self.name + ', 该爬虫因详情页获取失败被暂停'
+                    send_mail_when_error(msg)
+                pass
+
+            try:
+                items['web_time'] = each_li.xpath('./span[@class="time"]/text()').extract_first().strip()
+            except:
+                pass
+
+            yield scrapy.Request(items['url'], callback = self.parse_article, headers = self.headers, meta = {'items' : deepcopy(items)})
+
+
+    def parse_article(self, response):
+        items = response.meta['items']
+
+        if '招标公告' in items['title']:
+            items['type_id'] = '38255'
+        elif '变更' in items['title'] or '更正' in items['title']:
+            items['type_id'] = '38256'
+
+        try:
+            dirty_article = re.search(r'<div class="content_" id="zoom">(.*?)<!--底部-->', response.text, re.S).group(1)
+            clean_article = re.sub(self.regularExpression, ' ', dirty_article)
+            items['intro'] = clean_article
+        except:
+            pass
+
+        items['addr_id'] = '431'
+        items["source_name"] = '岳阳市人民政府'
+
+        yield items
+
+
+
--- china_canton_railway/spider_name.py
+++ china_canton_railway/spider_name.py
@@ -2,5 +2,6 @@ spiders_name = {
         # 'china_canton_railway_spider' : '中国铁路广州局集团有限公司物资采购商务平台',
         # 'china_railway_luban_spider' : '中国中铁采购电子商务平台',
         # 'AirChina_spider' : '中国国际航空公司',
-        'changjiang_waterway_spider' : '长江航道局'
+        # 'changjiang_waterway_spider' : '长江航道局',
+        'yueyang_city_gov_spider' : '岳阳市人民政府'
 }
