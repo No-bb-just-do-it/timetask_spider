9605b90abd29326b5c18898c23f2b2291d345ff9
winkboy <ramsey_leung@163.com>
2019-02-12 21:28:22 +0800

    新乡市已爬
    新乡市已爬

--- china_canton_railway/spider_name.py
+++ china_canton_railway/spider_name.py
@@ -10,9 +10,9 @@ spiders_name = {
         # 'longyan_city_gov_spider' : '眉山市公共资源交易网',              # 已爬
         # 'dalian_city_gov_spider' : '大连市公共资源',
         # 'foshan_nanhai_gov_spider' : '佛山市南海区人民政府网',           # 已爬
-        # 'xinxiang_city_gov_spider' : '新乡市政府采购网',
+        # 'xinxiang_city_gov_spider' : '新乡市政府采购网',                  # 已爬
         # 'liupanshui_city_gov_spider' : '六盘水市公共资源交易网',           # js加密 已爬
-        # 'jinzhou_city_gov_spider' : '锦州市公共资源交易管理办公室',
+        # 'jinzhou_city_gov_spider' : '锦州市公共资源交易管理办公室',         # 已爬
         # 'jingzhou_city_gov_spider' : '荆州市公共资源交易信息网',
         # 'anshun_city_gov_spider' : '安顺市全国公共资源交易平台',
         # 'hefei_city_gov_spider' : '合肥市政府信息公开网',               # 写着写着502错误
